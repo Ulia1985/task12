2915a0f55d57d16f0ac14589f93ed89b7f0301bf
Ulia <cs-gorodok@yandex.ru>
2023-11-07 11:56:22 +0400

    scr

--- src/test/java/PosterTest.java
+++ src/test/java/PosterTest.java
@@ -60,13 +60,12 @@ public class PosterTest {
     public void lessLimit() {
         Poster manager = new Poster();
 
-        manager.addMovie("Film I");
-        manager.addMovie("Film II");
         manager.addMovie("Film III");
+        manager.addMovie("Film IV");
+        manager.addMovie("Film V");
 
+        String[] expected = {"Film V", "Film IV", "Film III"};
         String[] actual = manager.findLast();
-        String[] expected = {"Film III", "Film II", "Film I"};
-
 
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -82,10 +81,12 @@ public class PosterTest {
         manager.addMovie("Film V");
         manager.addMovie("Film VI");
         manager.addMovie("Film VII");
+        manager.addMovie("Film VIII");
 
+        String[] expected = {"Film VIII", "Film VII", "Film VI", "Film V", "Film IV"};
         String[] actual = manager.findLast();
-        String[] expected = {"Film VII", "Film VI", "Film V", "Film IV", "Film III", "Film II", "Film I"};
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
 }
